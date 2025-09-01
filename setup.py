637d74336b198dd250aba8653246a3593112287d
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-09-01 21:29:40 +0200

    Move to setup.py, pyproject.toml is unusable when using

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension
+
+pykatex_ext = Extension(
+    "pykatex",
+    sources=[
+        "src/pykatex.c",
+        "src/katex.c", 
+        "src/quickjs/quickjs-libc.c",
+        "src/quickjs/quickjs.c",
+        "src/quickjs/libregexp.c",
+        "src/quickjs/libunicode.c",
+        "src/quickjs/cutils.c",
+        "src/quickjs/dtoa.c"
+    ],
+    include_dirs=["src/quickjs"],
+    define_macros=[
+        ("_GNU_SOURCE", None),
+        ("CONFIG_VERSION", "\"2025-04-26\"")
+    ]
+)
+
+setup(
+    ext_modules=[pykatex_ext]
+)
