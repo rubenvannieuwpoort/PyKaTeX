bcc25c693f48431f0a7683071b0fc842636113cd
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-10-06 10:45:50 +0200

    Set console.warn to avoid crashes

--- src/katex.js
+++ src/katex.js
@@ -2,6 +2,8 @@ import katex from "./katex/katex.mjs";
 
 function renderToString(input, options) {
 	// console.log("options =", JSON.stringify(options, null, 2));
+
+	console.warn = console.log;  // katex expects console.warn to work
 	return katex.renderToString(input, options);
 }
 
